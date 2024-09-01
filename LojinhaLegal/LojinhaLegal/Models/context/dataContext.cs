2a32305c19ba9ea43f20466dd2b67bbef0a0e30b
Ilan Wendling Thoele <ilanwendling@gmail.com>
2024-09-01 16:58:13 -0300

    build: add database environment

---
+++ LojinhaLegal/LojinhaLegal/Models/context/dataContext.cs
@@ -0,0 +1,20 @@
+﻿using LojinhaLegal.Models.entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojinhaLegal.Models.context
+{
+    public class dataContext : DbContext
+    {
+        public dataContext(DbContextOptions<dataContext> options)
+            : base(options)
+        {
+            if (Database.GetPendingMigrations().Any())
+            {
+                Database.Migrate();
+
+            }
+        }
+
+        public DbSet<Produto> ProdutoEntity { get; set; }
+    }
+}
---
+++ LojinhaLegal/LojinhaLegal/Models/entity/Produto.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace LojinhaLegal.Models.entity
+{
+    public class Produto
+    {
+        PrimaryKeyAttribute id;
+        string name;
+        string value;
+        string description;
+        public Produto() {
+            this.name = string.Empty;
+            this.value = string.Empty;
+            this.description = string.Empty;
+        }
+        public Produto(string name, string value, string description)
+        {
+            this.name = name;
+            this.value = value;
+            this.description = description;
+        }
+
+        public string Name
+        {
+            get { return this.name; } set { this.name = value; }
+        }
+
+
+        public string Value
+        {
+            get { return this.value; } set {this.value = value; }
+
+        }
+
+        public string Description
+        { get { return this.description; } set { this.description = value; } }
+
+
+    }
+}
